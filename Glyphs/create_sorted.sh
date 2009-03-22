2d82f24fbea52db6112ba4497dd25ea5c4642152
Erica Sadun <erica.sadun@arstechnica.com>
2009-03-21 20:16:10 -0600

    Preliminary data commit

---
+++ Glyphs/create_sorted.sh
@@ -0,0 +1,17 @@
+mkdir -p ZZApple
+mkdir -p ZZButterfly
+mkdir -p ZZFrog
+mkdir -p ZZHand
+mkdir -p ZZDaisy
+mkdir -p ZZLeaf
+mkdir -p ZZSmokeFace
+mkdir -p ZZSeahorse
+
+cp *Apple.jpg ZZApple
+cp *Butterfly.jpg ZZButterfly
+cp *Frog.jpg ZZFrog
+cp *Hand.jpg ZZHand
+cp *Daisy.jpg ZZDaisy
+cp *Leaf.jpg ZZLeaf
+cp *SmokeFace.jpg ZZSmokeFace
+cp *Seahorse.jpg ZZSeahorse
